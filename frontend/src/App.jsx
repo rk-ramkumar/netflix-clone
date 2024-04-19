0caa8411c5fb5d3b1eeb81bef741ff38c18312f2
ram <rramkumar1306@gmail.com>
2024-04-19 23:08:48 +0530

    Implemented sign in.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import SignIn from './components/SignIn'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
       <div>
-        NETFLICKS CLONE
+        <SignIn/>
       </div>
     </>
   )
--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <>
+      <div class="bg-loginBg w-screen h-screen backdrop-contrast-100">
+        <div class="absolute inset-0 bg-black opacity-50"></div>
+        <div className="flex justify-start uppercase text-5xl p-9 font-extrabold text-red-500 brightness-100">
+          Netflicks
+        </div>
+        <div className="flex-grow container w-[600px] mx-auto mt-10 px-4 sm:px-0 rounded-md backdrop-opacity-10 backdrop-invert bg-black/70">
+          <div className="max-w-3xl mx-auto flex-col p-6">
+            <h2 className="text-start font-bold text-4xl">Sign In</h2>
+            <div className=" mb-4">
+              <input
+                className="p-4 rounded-md"
+                placeholder="Email or phone number"
+              />
+            </div>
+            <div>
+              <input
+                className="p-4 rounded-md"
+                placeholder="Password"
+              />
+            </div>
 
-export default SignIn
+            <div className="flex-grow"><button>Sign In</button></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
