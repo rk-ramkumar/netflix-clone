808864d66f8d11c7f00ccd564daee58c37f1b7c4
ram <rramkumar1306@gmail.com>
2024-04-22 23:42:39 +0530

    Refactored.

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -10,21 +10,21 @@ const SignIn = () => {
         </div>
         <div className="flex-grow container w-[600px] mx-auto mt-10 px-4 sm:px-0 rounded-md backdrop-opacity-10 backdrop-invert bg-black/70">
           <div className="max-w-3xl mx-auto flex-col p-6">
-            <h2 className="text-start font-bold text-4xl">Sign In</h2>
+            <h2 className="text-start font-bold text-4xl mb-4">Sign In</h2>
             <div className=" mb-4">
               <input
-                className="p-4 rounded-md"
+                className="p-4 w-96 rounded-md bg-slate-300 text-black"
                 placeholder="Email or phone number"
               />
             </div>
-            <div>
+            <div className="mb-4">
               <input
-                className="p-4 rounded-md"
+                className="p-4 w-96 rounded-md bg-slate-300 text-black"
                 placeholder="Password"
               />
             </div>
 
-            <div className="flex-grow"><button>Sign In</button></div>
+            <div className=""><button className="w-96">Sign In</button></div>
           </div>
         </div>
       </div>
